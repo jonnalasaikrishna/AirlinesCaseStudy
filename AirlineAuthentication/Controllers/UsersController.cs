75529b36d004f5710fa79e166dcb4c90b6c946be
balujonnala <balujonnala222@gmail.com>
2022-05-05 20:50:08 +0530

    Add project files.

---
+++ AirlineAuthentication/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using AirlineAuthentication.Interface;
+using AirlineAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IJWTManagerRepository iJWTManager;
+
+        public UsersController(IJWTManagerRepository jWTManager)
+        {
+            iJWTManager = jWTManager;
+        }
+
+        
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("authenticate")]
+        public IActionResult Authenticate(Users userdata)
+        {
+            var token = iJWTManager.Authenticate(userdata);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+    }
+}
